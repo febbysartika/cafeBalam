2606ed67531ea4f46a7dadfc059e04163996dc7b
iwangnurhakiki <80211668+iwangnurhakiki@users.noreply.github.com>
2021-05-19 10:38:53 +0700

    Add files via upload

---
+++ models/Recep.js
@@ -0,0 +1,23 @@
+class Recep {
+    constructor(
+        id,
+        categoryId,
+        complexity,
+        imageUrl,
+        duration,
+        alamat,
+        ingredients,
+        steps,
+    ) {
+        this.id=id,
+        this.categoryId=categoryId,
+        this.complexity=complexity,
+        this.imageUrl=imageUrl,
+        this.duration=duration,
+        this.alamat=alamat,
+        this.ingredients=ingredients,
+        this.steps=steps
+      }
+}
+
+export default Recep
